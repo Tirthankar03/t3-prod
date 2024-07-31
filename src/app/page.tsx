ddda3031f51560929a9206e4f916e91b2d2bde93
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-08-01 05:24:05 +0530

    force dynamic + ts version setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = 'force-dynamic'
+
 const mockUrls = [
   "https://utfs.io/f/144b8144-39b9-46d5-91d3-e9ce33128f40-wzsr1w.jpg",
   "https://utfs.io/f/9c706f99-af2c-42d6-9a6c-367a85257218-2fof.png",
@@ -14,18 +16,17 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 export default async function HomePage() {
-
-  const posts = await db.query.posts.findMany()
-
+  
+  const posts = await db.query.posts.findMany();
 
   console.log(posts);
-  
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-      {posts.map((post) => (
-<div key={post.id}>{post.name}</div>
-))}
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
